a9aa79c6ba41b29a153b1b24a9e5e66d779d3d62
Gene Coetzee <straptiteusa@gmail.com>
2014-10-08 18:43:44 +0000

    Tracktite after rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tracktite::Application.config.secret_key_base = 'ed76d7bdb7498a815347d96241c5689081bb9adcfddea1cbc4d5d7f2ce1cb7d75e109527914bdc4e28798a494b614b32ac9b3de4475ecbd4645eb1e3a077fd42'
